310329dafa1e7357db7d098969474ff55d5d8430
unknown <ikbaal012@gmail.com>
2021-11-19 08:04:11 +0000

    Test

--- src/contact_info.php
+++ src/contact_info.php
@@ -1,9 +1,13 @@
 <?php
     //Load environment variables
+    echo "\n\nBefore Dotenv\Dotenv::createImmutable(...)";
     $dotenv = Dotenv\Dotenv::createImmutable(__DIR__."/..");
+    echo "\n\nBefore dotenvload->load";
     $dotenv->load();
+    echo "\n\nAfter dotenvload->load";
 
-    $myEmail = $_SERVER["MY_EMAIL"];
-    $myPass = $_SERVER["MY_PASS"];
-    $myPhone = $_SERVER["MY_PHONE"];
+    $myEmail = $_ENV["MY_EMAIL"];
+    echo "\n\nAfter env variable assignment";
+    $myPass = $_ENV["MY_PASS"];
+    $myPhone = $_ENV["MY_PHONE"];
 ?>

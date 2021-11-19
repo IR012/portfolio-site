50d592df452e893d5bf0061b5c8ed95f85d0984a
unknown <ikbaal012@gmail.com>
2021-11-19 10:44:33 +0000

    Update page contents

--- src/about.php
+++ src/about.php
@@ -7,14 +7,13 @@
                     <p>
                         Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.
                         Nunc viverra imperdiet enim. Fusce est. Vivamus a tellus.
-                        Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin pharetra nonummy pede. Mauris et orci.
+                        Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin pharetra nonummy pede. Mauris et orci. 
                     </p>
                 </div>
                 <div class="col-2">
                     <h2>Skills</h2>
                     <div class="box">
-                        <div class="box-item"><p>HTML</p></div>
-                        <div class="box-item"><p>CSS</p></div>
+                        <div class="box-item"><p>HTML & CSS</p></div>
                         <div class="box-item"><p>JavaScript</p></div>
                         <div class="box-item"><p>Express.js</p></div>
                         <div class="box-item"><p>Node.js</p></div>
@@ -22,6 +21,8 @@
                         <div class="box-item"><p>Python</p></div>
                         <div class="box-item"><p>Java</p></div>
                         <div class="box-item"><p>C#</p></div>
+                        <div class="box-item"><p>PHP</p></div>
+                        <div class="box-item"><p>SQL</p></div>
                     </div>
                 </div>
         </div>
--- src/notification.php
+++ src/notification.php
@@ -6,7 +6,7 @@
 ?>
 <div class="notification" style=<?= (!isset($_POST["notification-close"]) || (isset($_POST["notification-close"]) && $notificationClose != "close")) ? "" : "display:none" ?> >
     <h3>This website is still under development</h3>
-    <p>Email: ikbaal012@gmail.com to get in touch</p>
+    <p><a href="#contact">Send a message</a></p>
     <div class="close-button">
         <p>X</p>
     </div>

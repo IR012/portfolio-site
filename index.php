573da41254359450e3703c41746b83ac154d9ebf
unknown <ikbaal012@gmail.com>
2021-11-19 02:19:56 +0000

    Moved index.php and .htaccess out of src directory

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ikbaal Rehal</title>
+    <link rel="stylesheet" href="styles/styles.css">
+    <link rel="stylesheet" href="lib/swiper-bundle.css">
+    <!-- <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css"/> -->
+    <!-- <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script> -->
+    <script src="lib/swiper-bundle.js" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/9e605223dc.js" crossorigin="anonymous"></script>
+
+</head>
+<body>
+    <?php require_once __DIR__.'/vendor/autoload.php'; //Load Composer's autoloader?>
+    <?php require_once __DIR__.'/src/functions.php'; ?>
+    <?php require_once __DIR__.'/src/notification.php'; ?>
+    <?php require_once __DIR__.'/src/nav.php'; ?>
+    <?php require_once __DIR__.'/src/profile.php'; ?>
+    <?php require_once __DIR__.'/src/about.php'; ?>
+    <?php require_once __DIR__.'/src/portfolio.php'; ?>
+    <?php require_once __DIR__.'/src/contact.php'; ?>
+    <?php require_once __DIR__.'/src/footer.php'; ?>
+    <script src="scripts/app.js"></script>
+</body>
+</html>
